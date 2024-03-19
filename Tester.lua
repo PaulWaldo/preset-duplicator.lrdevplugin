30ab04f7a93d1b0e580097d587259edb30c96ca3
Paul Waldo <paul.w.waldo@gmail.com>
2024-03-19 16:39:53 -0400

    deeper testing

--- Tester.lua
+++ Tester.lua
@@ -1,9 +1,26 @@
+local Require = require "Require".path("/Users/paul/src/third-party/debuggingtoolkit.lrdevplugin").reload()
+local Debug = require "Debug".init()
+require "strict.lua"
+
 local LrDialogs = import 'LrDialogs'
 local LrApplication = import 'LrApplication'
+local LrDevelopController = import 'LrDevelopController'
+local LrTasks = import 'LrTasks'
 
-local num = #LrApplication.developPresetFolders()
-folders = LrApplication.developPresetFolders()
+-- Function to get a list of available develop presets
+local function getDevelopPresets()
+    local folders = {}
+    for _, folder in ipairs(LrApplication.developPresetFolders()) do
+        local presets = {}
+        local folderName = folder.getName()
+        for _, preset in ipairs(folder.getDevelopPresets()) do
+            folders[folderName] = folder.getDevelopPresets()
+        end
+    end
+    return folders
+end
 
-LrDialogs.message("Presets", "Found " .. num .. "presets", "info")
-LrDialogs.message("Presets Values", LrApplication.developPresetFolders(), "info")
-LrDialogs.message("LrApplication.developPresetManager()", LrApplication.developPresetManager(), "info")
+-- Entry point of the plugin
+LrTasks.startAsyncTask(Debug.showErrors(function()
+    LrDialogs.message("getDevelopPresets", getDevelopPresets(), "info")
+end))
